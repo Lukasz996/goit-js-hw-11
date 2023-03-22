a450419287cdd8d5204c25a5ada8dbc7cff86e12
Łukasz <lukaszkogut1996@gmail.com>
2023-03-22 21:26:15 +0100

    + task / test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+// import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+// const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = "29601510-cb62b0e552e92b2824471424b";
+const API_URL = `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+let page = 1;
+
+loadMoreBtn.style.display = 'none';
+
+
+
+
+Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
